fbd27109c29005e05115f02c39d1ba1ba353c468
Fabian Soriano <fabo.soriano@gmail.com>
2022-08-02 15:40:42 -0700

    added function for image and items

--- index.js
+++ index.js
@@ -1,6 +1,84 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+// *********************** //
+//Could not figure out the sky or grass, but including here for future reference.
+// *********************** //
+
+function tile(url, left, bottom, width, height){
+    for(let h = 0; h < height; h++){
+        for(let w = 0; w < width; w++){
+            newImage(url, left + w*100, bottom+h*100)
+        }
+    }
+}
+
+let horizon = window.innerHeight / 1.75
+let heightOfSky = window.innerHeight-horizon
+let heightOfGrass = horizon
+
+tile('assets/sky.png', 0, horizon, window.innerWidth/100, heightOfSky/100)
+tile('assets/grass.png', 0, 0, window.innerWidth/100, heightOfGrass/100)
+
+// *********************** //
+// function newImage()
+// *********************** //
+
+function newImage(url, left, bottom){
+    let image = document.createElement('img')
+    image.src = url
+    image.style.position = 'fixed'
+    image.style.left = left + 'px'
+    image.style.bottom = bottom + 'px'
+    document.body.append(image)
+    return image //this line was in the solution code, but it worked when I did not include this.
+}
+
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+
+// *********************** //
+// function for newItem()
+// *********************** //
+
+function newItem(url, left, bottom){
+    let image = newImage(url, left, bottom)
+
+    image.addEventListener('dblclick', function() {
+        image.remove()
+    })
+}
+
+newItem('assets/sword.png', 500, 405)
+newItem('assets/sheild.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
+
+// let sword = document.createElement('img')
+// sword.src = 'assets/sword.png'
+// sword.style.position = 'fixed'
+// sword.style.left = '500px'
+// sword.style.bottom = '405px'
+// document.body.append(sword)
+
+// sword.addEventListener('dblclick', function(){
+//     sword.remove()
+// })
+
+
+//Starter code
+
+// let greenCharacter = document.createElement('img')
+// greenCharacter.src = 'assets/green-character.gif'
+// greenCharacter.style.position = 'fixed'
+// greenCharacter.style.left = '100px'
+// greenCharacter.style.bottom = '100px'
+// document.body.append(greenCharacter)
+
+//Adding tree Element
+// let pineTree = document.createElement('img')
+// pineTree.src = 'assets/pine-tree.png'
+// pineTree.style.position = 'fixed'
+// pineTree.style.left = '450px'
+// pineTree.style.bottom = '200px'
+// document.body.append(pineTree)
